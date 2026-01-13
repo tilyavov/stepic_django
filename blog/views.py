82cc3ab421bd2ba24f61ccd8763e3cb7f84b17a7
Alisher <117917625+tilyavov@users.noreply.github.com>
2026-01-13 16:05:23 +0500

    Передача данных в шаблоны

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'my_name': 'Alisher',
+        'my_age': 22
+    }
+    return render(request, 'blog/index.html', context)
 
