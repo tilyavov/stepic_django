a42086105dc948a0facb4a89c2befcc353125c89
Alisher <117917625+tilyavov@users.noreply.github.com>
2026-01-14 08:37:10 +0500

    Тег для вывода информации в цикле

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    data = {"age": 50}
-    return render(request, 'blog/index.html', context=data)
+    cat = ["Python", "Java", "JS", "Go", "C#", "Kotlin"]
+    return render(request, 'blog/index.html', context={'cat': cat})
 
