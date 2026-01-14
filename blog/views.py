87a87cf9e5454655624cb79fcb88861f42e25f49
Alisher <117917625+tilyavov@users.noreply.github.com>
2026-01-14 08:33:08 +0500

    Тег для вывода текущих даты и времени

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    name_is_company = ['Tajiairnavigation', 'SomonAir']
-    carscompany = 'Audi'
-    context = {
-        'company': name_is_company,
-        'car': carscompany
-    }
-    return render(request, 'blog/index.html', context)
+    pass
+    return render(request, 'blog/index.html')
 
