10336d7972a66a32afc4f967710efaa752b571c9
Alisher <117917625+tilyavov@users.noreply.github.com>
2026-01-15 09:02:12 +0500

    Статичные файлы

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 
 def index(request):
-    return render(request, 'blog/index.html', context={"body": "<h1>Hello World!</h1>"})
+    data = {'name': 'Дмитрий', 'age': 39, 'phone': '+79123456789', 'email': 'dmitry@email.com'}
+    return TemplateResponse(request, 'blog/index.html', context = data)
 
